d3e161274dfe052ddca9d4b797dc0f2c2a419aa4
maldestor95 <maldestor95@gmail.com>
2021-03-12 08:03:15 +0100

    clean code

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img alt="Vuetify Logo" class="shrink mr-2" contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" transition="scale-transition" width="40" 
-          @click="menu='initial'"
-          />
-      </div>
-
-      <v-spacer></v-spacer>
-      <v-btn @click="menu='diagramme'"> Diagramme </v-btn>
-      <v-btn @click="menu='recettes'"> Recettes </v-btn>
+      <v-menu offset-y>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  color="primary"
+                  dark
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  Menu
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item>
+                  <v-list-item-title @click="menu='recettes'">recettes</v-list-item-title>
+                </v-list-item>
+                <v-list-item>
+                  <v-list-item-title @click="menu='initial'">Home</v-list-item-title>
+                </v-list-item>
+              </v-list>
+          </v-menu>
     </v-app-bar>
 
     <v-main>
+      
       <diagrammeTendance v-if="menu=='diagramme'" />
-      <fetchUrl v-if="menu=='initial'" />
       <recettes  v-if="menu=='recettes'" />
 
     </v-main>
@@ -24,7 +34,6 @@
 
 <script>
   import diagrammeTendance from './components/diagramme';
-  import fetchUrl from './components/fetchurl.vue';
   import recettes from './features/recette/recettes'
 
   export default {
@@ -32,7 +41,6 @@
 
     components: {
       diagrammeTendance,
-      fetchUrl,
       recettes
     },
 
